cfc622990552d7fa375361ccaad9e48b5f5befed
Snehas87 <hurkat.sneha@gmail.com>
2026-04-22 20:06:38 +0800

    Add files via upload

---
+++ print.py
@@ -0,0 +1,12 @@
+#print sentence
+print("Welcome to coding world with codingal")
+
+#print number
+print(10)
+ 
+print("Hello Nihit \n Welcome to coding world with codingal")
+
+print("Hello", 5)
+
+#end argument
+print("Welcome to", end = " *" )
---
+++ variable.py
@@ -0,0 +1,9 @@
+X = 5
+Y = "Nihit"
+
+print(X)
+print(Y)
+
+
+name = "Nihit"
+print("Hello, " + name + "! Welcome to coding world with codingal.")
