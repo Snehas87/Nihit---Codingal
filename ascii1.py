c53d2bfb805cfec845a1130bbf2594b8e2a6a87e
Snehas87 <hurkat.sneha@gmail.com>
2026-06-14 13:02:53 +0800

    Add files via upload

---
+++ ascii1.py
@@ -0,0 +1,12 @@
+print(ord('A'))
+
+print(ord('a'))
+
+print(ord('0'))
+
+print(ord('@'))
+
+
+print(chr(65))
+
+print(chr(97))
---
+++ ascii2.py
@@ -0,0 +1,24 @@
+char = input("Enter a single character:")
+
+if type(char) is str and len(char) == 1:
+    print("Valid Input")
+else:
+    print("Please enter exactly ONE character!")
+
+
+ascii_val = ord(char)
+
+print("character is :",char)
+print ("ASCII value of character is :",ascii_val)
+
+if ascii_val >= 65 and ascii_val <= 90:
+    print("Type: Uppercase Letter")
+elif ascii_val >= 97 and ascii_val <= 122:
+    print("Type: Lowercase Letter")
+elif ascii_val >= 48 and ascii_val <= 57:
+    print("Type: Digit")
+elif ascii_val == 32:
+    print("Type: Space")
+else:
+    print("Type: Special character")
+
